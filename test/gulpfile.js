73270feab0f244438a9d25a3c0f1389fa162cd04
Rowan Potgieter <rowanp@sahomeloans.com>
2017-06-29 11:28:32 +0200

    Added non-hub error

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -1,3 +1,16 @@
 var gulp = require('gulp')
 
 require('../')({gulp: gulp})
+var shell = require('../modules/common/shell.js')
+
+function lsCommand () {
+  var cmd = 'ls -l'
+  return shell.command(cmd)
+}
+
+function whichCommand () {
+  var cmd = 'which badFileName'
+  return shell.command(cmd)
+}
+
+gulp.task('cmd-error-non-hub', gulp.series(lsCommand, whichCommand))
